e87f62d5b42f3e59de613639af352c122b8854cf
Per Larsson <per.larsson@ub.gu.se>
2017-10-18 09:40:56 +0200

    Add controller and model etc for patron

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  namespace :api, :defaults => {:format => :json} do
+    post 'patrons', to: 'patrons#create'
+  end
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount_ember_app :frontend, to: "/"
+
 end
