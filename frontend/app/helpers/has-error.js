bc01f9aed30b4f286a5f11eae2e4ed356418c434
Lars Berglén <lars.berglen@ub.gu.se>
2017-10-24 13:32:56 +0200

    moved communication pref drop down

--- frontend/app/helpers/has-error.js
+++ frontend/app/helpers/has-error.js
@@ -1,13 +1,13 @@
 import Ember from 'ember';
 
-export function hasError(params/*, hash*/) {
+export function hasError(params /*, hash*/ ) {
   let errors = params[0];
-  
+
   if (errors) {
 
     let field = params[1];
     if (errors.findBy('field', field)) {
-       return 'has-error';
+      return 'has-error';
     }
     return 'has-success';
   }
