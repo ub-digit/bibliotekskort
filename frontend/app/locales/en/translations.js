2550a83a199c2bfc97d5cb6fc95a21450e4c6c73
Lars Berglén <lars.berglen@ub.gu.se>
2017-10-31 14:26:41 +0100

    Translatable page title

--- frontend/app/locales/en/translations.js
+++ frontend/app/locales/en/translations.js
@@ -11,7 +11,7 @@ export default {
   },
 
   "application": {
-
+    "title": "Library card"
   },
   "patron": {
     "header": 'Ansökan om bibliotekskort',
--- frontend/app/locales/sv/translations.js
+++ frontend/app/locales/sv/translations.js
@@ -11,6 +11,7 @@ export default {
   },
 
   "application": {
+    "title": "Bibliotekskort"
 
   },
   "patron": {
