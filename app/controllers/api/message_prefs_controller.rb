2421a2ea3db6550a293e5b3a9405730cfe7e8ec9
Magnus Kull <magnus.kull@ub.gu.se>
2017-12-06 15:54:17 +0100

    swedish and english texts

--- app/controllers/api/message_prefs_controller.rb
+++ app/controllers/api/message_prefs_controller.rb
@@ -2,9 +2,9 @@ class Api::MessagePrefsController < Api::ApiController
 
   def index
     @response[:message_prefs] = [
-      {id: "email", label_sv: 'Epost', label_en: 'Epost - en'},
-      {id: "sms_email", label_sv: 'Sms och epost', label_en: 'Sms och epost - en'},
-      {id: "sms", label_sv: 'Sms', label_en: 'Sms - en'},
+      {id: "email", label_sv: 'E-post', label_en: 'E-mail'},
+      {id: "sms_email", label_sv: 'E-post och SMS', label_en: 'E-mail and SMS'},
+      {id: "sms", label_sv: 'SMS', label_en: 'SMS'},
     ]
     render_json
   end
