91dec5cf48837ed6a80158b15b78dcbad5bcea0c
Per Larsson <per.larsson@ub.gu.se>
2024-11-12 19:29:43 +0100

    KOHA-1956 Update patron category dropdown

--- app/models/category_code.rb
+++ app/models/category_code.rb
@@ -1,11 +1,9 @@
 class CategoryCode
   def self.all
     [
-     {id: "EX", label_sv: "Allmänhet, bosatt i Sverige", label_en: "Member of the public, living in Sweden"},
-     {id: "UX", label_sv: "Allmänhet, bosatt utomlands", label_en: "Member of the public, living abroad"},
+     {id: "EX", label_sv: "Allmänhet", label_en: "Member of the public"},
      {id: "SR", label_sv: "Student vid annan högskola än GU", label_en: "Student at another university"},
      {id: "FR", label_sv: "Forskare vid annan högskola än GU", label_en: "Researcher at another university"},
-     {id: "SU", label_sv: "Internationell student vid GU", label_en: "International student at GU"},
      {id: "FX", label_sv: "Internationell gästforskare vid GU", label_en: "International visiting researcher at GU"},
      {id: "S",  label_sv: "Student vid GU", label_en: "Student at GU"},
      {id: "GU", label_sv: "Personal vid GU", label_en: "Staff at GU"}
